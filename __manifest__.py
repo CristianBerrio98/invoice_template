5c78501e13659f3cf3eb32814793c73fc8798f71
Cristian Berrio <cristian.berrio@proactivo.digital>
2024-01-03 08:18:00 -0500

    author

--- __manifest__.py
+++ __manifest__.py
@@ -1,14 +1,17 @@
 {
-    'name': 'Invoice Report Modulo',
+    'name': 'invoice_report',
+    'description': """
+        Get all TRM by date
+    """,
+    'author': "Cristian Berrio",
     'version': '1.0',
-    'category': 'Reports',
+    'category': 'Invoice',
     'depends': ['base', 'account'],
     'data': [
         'views/report_invoice_with_payments.xml',
     ],
     'installable': True,
-    'auto_install': True,
-    'application': False,
+    'auto_install': False,
     'sequence': 1,
     'demo': [],
     'qweb': [
