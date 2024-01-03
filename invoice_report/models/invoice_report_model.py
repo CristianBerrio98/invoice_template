805155ad7a169fd146599ecad4e6a80ba0686f35
Cristian Berrio <cristian.berrio@proactivo.digital>
2024-01-03 11:57:47 -0500

    new model

--- invoice_report/models/invoice_report_model.py
+++ invoice_report/models/invoice_report_model.py
@@ -2,3 +2,6 @@ from odoo import models, fields, api
 
 class AccountInvoice(models.Model):
     _inherit = 'account.move'
+    _name = "account.invoice"
+
+    transaction_ids_invoice = fields.Many2many('account.invoice', string='Transaction IDs')
