37d6cb2f5edad6abb8ced333ec10e4f06973209a
Cristian Berrio <cristian.berrio@proactivo.digital>
2024-01-03 12:03:41 -0500

    invoice

--- invoice_report/models/account_move.py
+++
@@ -1,6 +0,0 @@
-from odoo import models, fields, api
-
-class AccountMove(models.Model):
-    _inherit = 'account.move'
-
-    transaction_ids_move = fields.Many2many('account.invoice', string='Transaction IDs')
